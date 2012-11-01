409b807e6512fcdfb2accb942d8037dcc9c05a4d
System Administrator <root@mritzmacbookpro.chathamfinancial.com>
2012-11-01 13:23:08 -0400

    added pics

--- plate/public/js/custom.js
+++ plate/public/js/custom.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
   console.log('overwriting carousel');
   $('#mycarousel').carousel({interval: 4000});
+  $('ul.nav > li').click(function (e) {
+    e.preventDefault();
+    $('ul.nav > li').removeClass('active');
+    $(this).addClass('active');                
+  });     
 });
